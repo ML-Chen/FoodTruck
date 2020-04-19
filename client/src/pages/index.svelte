0c6392f2988e88d88ac22696341200e062b4f3c2
Michael Chen <michael.ld.chen@gmail.com>
2020-04-19 18:50:12 -0400

    Fix index.svelte

--- client/src/pages/index.svelte
+++ client/src/pages/index.svelte
@@ -13,9 +13,9 @@
 
 <p>The best app for buying food from food trucks (or even managing your food truck)! Cybernetically powered by Svelte, Routify, Flask, and MySQL.</p>
 
-{#if !userType}
+{#if !$userType}
     <p>Why don't you <a href={$url('../login')}>log in</a> or <a href={$url('../register')}>create an account</a>?</p>
-{:else if userType === 'Staff'}
+{:else if $userType === 'Staff'}
     <p>Hi staff member, you’re doing great work!</p>
 {:else}
     <p>Check out your slick <a href={$url('../home')}>personal dashboard</a>!</p>
