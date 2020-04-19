d37365c45d4e1af0100aa034a4c872f9a22a599f
Michael Chen <michael.ld.chen@gmail.com>
2020-04-19 04:32:26 -0400

    Edit index.svelte

--- client/src/pages/index.svelte
+++ client/src/pages/index.svelte
@@ -1,5 +1,22 @@
 <script>
-    import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
+    import { userType } from './_store.js';
+    import { url } from '@sveltech/routify';
+
+    userType.useLocalStorage();
 </script>
 
-<RoutifyIntro />
+<svelte:head>
+    <title>Food Truck</title>
+</svelte:head>
+
+<h1>Food Truck</h1>
+
+<p>The best app for buying food from food trucks (or even managing your food truck)! Cybernetically powered by Svelte, Routify, Flask, and MySQL.</p>
+
+{#if !userType}
+    <p>Why don't you <a href={$url('../login')}>log in</a> or <a href={$url('../register')}>create an account</a>?</p>
+{:else if userType === 'Staff'}
+    <p>Hi staff member, you’re doing great work!</p>
+{:else}
+    <p>Check out your slick <a href={$url('../home')}>personal dashboard</a>!</p>
+{/if}
