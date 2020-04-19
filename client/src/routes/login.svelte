c3f06fa2ecd757a69802e8f6e88ffd3a1131c8d1
Michael Chen <michael.ld.chen@gmail.com>
2020-04-18 22:49:32 -0400

    Start login page

--- client/src/routes/login.svelte
+++ client/src/routes/login.svelte
@@ -1,8 +1,29 @@
 <!-- Screen 1: Login -->
 
 <script>
+    import { setContext } from 'svelte';
+
     export let username;
     export let password;
+
+    export async function handleLogin() {
+        const response = await this.fetch('http://127.0.0.1:5000/login', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        });
+        const { token, userType, error } = await response.json();
+        if (!error) {
+            setContext('token', token);
+            setContext('userType', userType);
+        } else {
+            console.log(error);
+            alert(error);
+        }
+    }
 </script>
 
 <svelte:head>
@@ -11,17 +32,11 @@
 
 <h1>GT Food Truck</h1>
 
-<form class="form">
+<form class="form" on:submit|preventDefault={handleLogin}>
     <label for="username">Username:</label>
     <input type="text" id="username" name="username" bind:value={$username} />
     <label for="password">Password:</label>
     <input type="text" id="password" name="password" bind:value={$password} />
+    <button type="submit">Login</button>
+    <button type="button">Register</button>
 </form>
-
-<style>
-  .form {
-    display: grid;
-    grid-template-columns: 20% 80%;
-    grid-column-gap: 10px;
-  }
-</style>
