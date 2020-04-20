f89dc76c045ec08d053cb26056731768b2f58cda
Duncan <duncansiebert2@gmail.com>
2020-04-20 00:03:06 -0400

    Css for create food added

---
+++ client/src/pages/admin/create-food.svelte
@@ -0,0 +1,27 @@
+<!-- Screen 10: Create Food -->
+
+import { url, goto } from '@sveltech/routify';
+    import { token, userType } from '../_store.js';
+    import axios from 'axios';
+
+    token.useLocalStorage();
+    userType.useLocalStorage();
+
+    </script>
+
+<svelte:head>
+    <title>Create Food</title>
+</svelte:head>
+
+
+<h1>Create Food</h1>
+
+<form on:submit|preventDefault={createBuilding}>
+    <label for="FoodName">Name</label>
+    <input type="text" id="FoodName" name="FoodName" bind:value={FoodName} />
+
+    
+    <button type="submit">Create</button>
+</form>
+
+<a href={$url('../../home')}>Back</a>
