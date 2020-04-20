f96643c55baa0ac71a210e1b6bc0a7085978dc1b
Duncan <duncansiebert2@gmail.com>
2020-04-19 22:57:13 -0400

    adds fetch and update functionality

--- client/src/pages/admin/update-building.svelte
+++ client/src/pages/admin/update-building.svelte
@@ -13,7 +13,25 @@
     let tags = [];
     let wipTag;
     let errorMsg;
-    onMount(updateBuilding)
+    onMount(FetchBuilding)
+    async function fetchBuildings() {
+        try {
+            const json = (await axios.get('http://localhost:4000/ad_filter_building_station', {
+                params: { buildingName, stationName, buildingTag, minCapacity, maxCapacity, errorMsg, selectedBuilding, token: $token }
+            })).data;
+            if (json.error) {
+                errorMsg = json.error
+            } else {
+                /** @type {[{buildingName: string, tags: string, stationName: string, capacity: int, foodTruckNames: string}]} */
+                buildings = json.filter(building => Object.keys(building).length !== 0);
+            }
+            errorMsg = null;
+            errorMsg2 = null;
+        } catch (error) {
+            console.log(error);
+            errorMsg = 'Network error. Maybe the server is down?';
+        }
+    }
     async function updateBuilding() {
         try {
             const json = (await axios.post('http://localhost:4000/ad_update_building', { buildingName, description, token: $token })).data;
