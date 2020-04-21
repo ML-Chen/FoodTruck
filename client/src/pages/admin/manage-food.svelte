5ee58d0c727fa3c7f31949e3bb8fd9c4c8cb10e8
Michael Chen <michael.ld.chen@gmail.com>
2020-04-21 01:19:37 -0400

    Fix undefined food name

--- client/src/pages/admin/manage-food.svelte
+++ client/src/pages/admin/manage-food.svelte
@@ -19,7 +19,6 @@
 
     // Form values (all strings)
     let errorMsg;
-    let errorMsg2;
     let foodNameFilter;
     let sortedBy = 'name'; // choices: ['name', 'menuCount', 'purchaseCount']
     let sortDirection = 'ASC'; // choices: ['ASC', 'DESC']
@@ -32,7 +31,8 @@
             foods = (await axios.get('http://localhost:4000/ad_filter_food', {
                 params: { foodName: foodNameFilter, sortedBy, sortDirection, token: $token }
             })).data;
-            } catch (error) {
+            errorMsg = null;
+        } catch (error) {
             console.log(error.response.data)
             errorMsg = error.response.data.error;
         }
@@ -68,76 +68,36 @@
 <form on:submit|preventDefault={fetchFoods}>
     <label for="food-name">Name</label>
     <input type="text" id="food-name" name="food-name" bind:value={foodNameFilter} />
-
-    <label for="sort-by">Sort by:</label>
-    <select id="sort-by" bind:value={sortedBy}>
-        <option value="name">Name</option>
-        <option value="menuCount">Menu count</option>
-        <option value="purchaseCount">Purchase count</option>
-    </select>
-
-    <label for="sort-dir">Sort by:</label>
-    <select id="sort-dir" bind:value={sortDirection}>
-        <option value="ASC">Ascending</option>
-        <option value="DESC">Descending</option>
-    </select>
-
-    <br>
+    <br />
     <button type="submit">Filter</button>
 </form>
 
 <form on:submit|preventDefault={deleteFood}>
-<!--    <table>
-        <thead>
-            <tr>
-                <td>Name</td>
-                <td>Menu Count</td>
-                <td>Purchase Count</td>
-            </tr>
-        </thead>
-        <tbody>
-            {#if foods}
-                {#each foods as food}
-                    <tr>
-                        <td>
-                            <label>
-                                <input type="radio" bind:group={selectedFoodName} value={food.foodName}/>
-                                {food.foodName}
-                            </label>
-                        </td>
-                        <td>{food.menuCount}</td>
-                        <td>{food.purchaseCount}</td>
-                    </tr>
-                {/each}
-            {/if}
-        </tbody>
-    </table>
-    {#if errorMsg}
-        <p class="error">{errorMsg}</p>
-    {/if}  -->
     
     <TableSort items={foods}>
-    <tr slot="thead">
-        <th></th>
-        <th data-sort="name">Name</th>
-        <th data-sort="menuCount">Menu Count</th>
-        <th data-sort="purchaseCount">Purchase Count</th>
-    </tr>
-    <tr slot="tbody" let:item={food}>
-        <input type="radio" bind:group={selectedFoodName} value={food.foodName} />
-        <td>{food.name}</td>
-        <td>{food.menuCount}</td>
-        <td>{food.purchaseCount}</td>
-    </tr>
-</TableSort>
+        <tr slot="thead">
+            <th></th>
+            <th data-sort="foodName">Name</th>
+            <th data-sort="menuCount">Menu Count</th>
+            <th data-sort="purchaseCount">Purchase Count</th>
+        </tr>
+        <tr slot="tbody" let:item={food}>
+            <input type="radio" bind:group={selectedFoodName} value={food.foodName} />
+            <td>{food.foodName}</td>
+            <td>{food.menuCount}</td>
+            <td>{food.purchaseCount}</td>
+        </tr>
+    </TableSort>
 
     <a href={$url('../create-food')}>Create food</a>
     <button type="submit" on:click={deleteFood}>Delete food</button>
 </form>
 
+{#if errorMsg}
+    <p style="error">{errorMsg}</p>
+{/if}
 
 <a href={$url('../../home')}>Back</a>
-
 
 <style>
     .error {
