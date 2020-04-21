c9aafe7245e6c8d1a06c069265b5f54035ddd147
atrotter99 <63320500+atrotter99@users.noreply.github.com>
2020-04-20 20:14:41 -0400

    fix create station

--- client/src/pages/admin/create-station.svelte
+++ client/src/pages/admin/create-station.svelte
@@ -1,1 +1,53 @@
 <!-- Screen #7: Admin Create Station -->
+
+<script>
+    import { url, goto } from '@sveltech/routify';
+    import { token, userType } from '../_store.js';
+    import axios from 'axios';
+    token.useLocalStorage();
+    userType.useLocalStorage();
+    let stationName;
+    let capacity;
+    let buildingName;
+    let errorMsg;
+    async function createStation() {
+        if (!stationName) {
+            errorMsg = 'Station name must not be blank';
+        } else if (!capacity) {
+            errorMsg = 'Capacity must not be blank';
+        } else {
+            try {
+                const json = (await axios.post('http://localhost:4000/ad_create_station', { stationName, capacity, token: $token })).data;
+                    if (json.error) {
+                        errorMsg = json.error;
+                    }
+            } catch (error) {
+                console.log(error.response.data)
+                errorMsg = error.response.data.error;
+            }
+        }
+    }
+</script>
+
+<svelte:head>Create Station</svelte:head>
+
+<h1>Create Station</h1>
+
+<form on:submit|preventDefault={createStation}>
+    <label for="stationName">Name</label>
+    <input type="text" id="stationName" name="stationName" bind:value={stationName} />
+
+    <label for="capacity">Description</label>
+    <textarea id="capacity" name="capacity" bind:value={capacity} />
+
+    <label for="building">Sponsored Building</label>
+    <select id="building-name" name="building-name" bind:value={buildingName}>
+        {#if buildings}
+            {#each [null].concat(buildings.map(building => building.buildingName)) as bName}
+                <option value={bName} selected={bName === buildingName}>{bName || ''}</option>
+            {/each}
+        {/if}
+    <button type="submit">Create</button>
+</form>
+  
+<a href={$url('../../home')}>Back</a>
